714c5f6c82bbdb428d403a9f4d782ef3fb3bc540
smitaldesai <desaismital@gmail.com>
2017-03-18 15:10:44 +0530

    [Smital] check login status and provide approapriate options

--- Blue_Yellow/controllers/base_controller.py
+++ Blue_Yellow/controllers/base_controller.py
@@ -5,3 +5,7 @@ class BaseController:
     def __init__(self, request):
         self.request = request
         self.build_cache_id = cache.build_cache_id
+
+    @property
+    def is_logged_in(self):
+        return False
