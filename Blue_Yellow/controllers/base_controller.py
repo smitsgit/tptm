630418bdfedcea3e6ac4f3639656269b143c31fb
smitaldesai <desaismital@gmail.com>
2017-03-18 17:02:02 +0530

    [Smital] get the dhared layout working. define macro - define slots attach macro to controller then use macro and use slot

--- Blue_Yellow/controllers/base_controller.py
+++ Blue_Yellow/controllers/base_controller.py
@@ -1,10 +1,17 @@
 import Blue_Yellow.infrastructure.static_cache as cache
+import pyramid.renderers
 
 
 class BaseController:
     def __init__(self, request):
         self.request = request
         self.build_cache_id = cache.build_cache_id
+
+        layout_renderer = pyramid.renderers.get_renderer('Blue_Yellow:templates/shared/_layout.pt')
+        impl = layout_renderer.implementation()
+        self.common_layout = impl['common_layout']
+        # self.additional_css = impl['additional_css']
+        # self.additional_js = impl['additional_js']
 
     @property
     def is_logged_in(self):
