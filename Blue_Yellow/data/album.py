ba7ac1048a0a93f2608d1dcd95f863332b92ebd5
smitaldesai <desaismital@gmail.com>
2017-03-21 19:43:08 +0530

    [Smital] create the indexes for faster queries and add uniqueness

--- Blue_Yellow/data/album.py
+++ Blue_Yellow/data/album.py
@@ -8,9 +8,9 @@ from  Blue_Yellow.data.modelbase import SqlAlchemyBase
 class Album(SqlAlchemyBase):
     __tablename__ = 'Album'
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    name = sqlalchemy.Column(sqlalchemy.String)
-    year = sqlalchemy.Column(sqlalchemy.Integer)
-    price = sqlalchemy.Column(sqlalchemy.Float)
+    name = sqlalchemy.Column(sqlalchemy.String, index=True, unique=True, nullable=False)
+    year = sqlalchemy.Column(sqlalchemy.Integer, index=True)
+    price = sqlalchemy.Column(sqlalchemy.Float, index=True)
     album_image = sqlalchemy.Column(sqlalchemy.String)
     # Name of the table and back_populates = field
     tracks = sqlalchemy.orm.relationship('Track', back_populates='album',
--- Blue_Yellow/data/track.py
+++ Blue_Yellow/data/track.py
@@ -6,11 +6,11 @@ from Blue_Yellow.data.modelbase import SqlAlchemyBase
 class Track(SqlAlchemyBase):
       __tablename__ = 'Track'
       id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-      name = sqlalchemy.Column(sqlalchemy.String)
+      name = sqlalchemy.Column(sqlalchemy.String, index=True, unique=True, nullable=False)
       length = sqlalchemy.Column(sqlalchemy.Integer)
       audio_url = sqlalchemy.Column(sqlalchemy.String)
       video_url = sqlalchemy.Column(sqlalchemy.String)
-      display_order = sqlalchemy.Column(sqlalchemy.Integer)
+      display_order = sqlalchemy.Column(sqlalchemy.Integer, index=True)
 
       album_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('Album.id'))
                                           # Name of the table and back_populates = field
